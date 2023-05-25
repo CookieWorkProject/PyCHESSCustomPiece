382e0a9bc3957a24d8e0a1f541f98cc79c6aad59
PapiChuloXX <afrokid010404@gmail.com>
2023-05-25 12:34:18 -0500

    changes

--- board.py
+++ board.py
@@ -29,24 +29,6 @@ for row in range(8):
             square = Squares(column * 64, row * 64, '#9D5A30')
         square_group.add(square)
 
-bK = King('bK.png', 'black')
-bQ = Queen('bQ.png', 'black')
-bB = Bishop('bB.png', 'black')
-bN = Knight('bN.png', 'black')
-bR = Castle('bR.png', 'black')
-bp = Pawn('bp.png', 'black')
-
-wK = King('wK.png', 'white')
-wQ = Queen('wQ.png', 'white')
-wB = Bishop('wB.png', 'white')
-wN = Knight('wN.png', 'white')
-wR = Castle('wR.png', 'white')
-wp = Pawn('wp.png', 'white')
-
-
-piece_list = [bK, bQ, bB, bN, bR, bp, wK, wQ, wB, wN, wR, wp]
-
-
 global black_location
 black_location = set()
 
@@ -59,6 +41,17 @@ Player_click = []
 global gs
 gs = Gs()
 
+for i, row in enumerate(gs.board):
+    for x,col in enumerate(row):
+         if col != "--":
+            if gs.board[i][x].color == "black":
+                if isinstance(gs.board[i][x], King):
+                    gs.blackKing = (i, x)
+                gs.black_pieces.append(gs.board[i][x])
+            else:
+                if isinstance(gs.board[i][x], King):
+                    gs.whiteKing = (i, x)
+                gs.white_pieces.append(gs.board[i][x])
 turn = "white"
 
 move_sound = pg.mixer.Sound("move-self.mp3")
@@ -68,39 +61,39 @@ def draw_pieces(screen,gs):
     for row in range(8):
         for column in range(8):
             if gs.board[row][column] != "--":
-                if gs.board[row][column] == "bp":
+                if gs.board[row][column].color == "black":
                     pos = (column * 64, row * 64)
-                    screen.blit(bp.image, pos)
+                    screen.blit(gs.board[row][column].image, pos)
                     black_location.add((pos[1] // 64 , pos[0] // 64))
 
-                elif gs.board[row][column] == "wp":
+                elif gs.board[row][column].color == "white":
                     pos = (column * 64, row * 64)
-                    screen.blit(wp.image, pos)
+                    screen.blit(gs.board[row][column].image, pos)
                     white_location.add((pos[1] // 64, pos[0] // 64))
 
-                elif gs.board[row][column][0] == "b":
-                    pos = (column * 64, row * 64)
-                    screen.blit(pg.image.load(gs.board[row][column] + '.png'), pos)
-                    black_location.add((pos[1] // 64 , pos[0] // 64))
-
-                else:
-                    pos = (column * 64, row * 64)
-                    screen.blit(pg.image.load(gs.board[row][column] + '.png'), pos)
-                    white_location.add((pos[1] // 64, pos[0] // 64))
 
 def move_piece(Player_click, gs):
     piece_selected, desired_move = Player_click
-    if eval(gs.board[piece_selected[0]][piece_selected[1]]) in piece_list:
-        if desired_move in eval(gs.board[piece_selected[0]][piece_selected[1]]).get_moves(piece_selected, gs.board):
-            tmp = gs.board[piece_selected[0]][piece_selected[1]]
-            gs.board[piece_selected[0]][piece_selected[1]] = "--"
-            gs.board[desired_move[0]][desired_move[1]] = tmp
-            move_sound.play()
-            circle_group.empty()
+    if desired_move in white_location:
+        white_location.remove(desired_move)
+    elif desired_move in black_location:
+        black_location.remove(desired_move)
 
-            return True
+    if desired_move in gs.board[piece_selected[0]][piece_selected[1]].get_valid_moves(piece_selected, gs, turn):
+        tmp = gs.board[piece_selected[0]][piece_selected[1]]
+        gs.board[piece_selected[0]][piece_selected[1]] = "--"
+        captured = gs.board[desired_move[0]][desired_move[1]]
+        gs.board[desired_move[0]][desired_move[1]] = tmp
+        move_sound.play()
+        circle_group.empty()
+
+        if captured != "--":
+            if captured.color == "white":
+                gs.white_pieces.remove((captured))
+            elif captured.color == "black":
+                gs.black_pieces.remove((captured))
+        return True
     return False
-
 
 
 while flag:
@@ -108,60 +101,72 @@ while flag:
         if event.type == pg.QUIT:
             flag = False
         if event.type == pg.MOUSEBUTTONDOWN:
-           y_pos, x_pos = pg.mouse.get_pos()
+            y_pos, x_pos = pg.mouse.get_pos()
 
-           if Selected == (x_pos//64, y_pos//64):
+            if Selected == (x_pos//64, y_pos//64):
+                    Selected = ()
+                    Player_click = []
+                    circle_group.empty()
+            else:
+                    if len(Player_click) == 0:
+                        if turn == 'white' and (x_pos//64, y_pos//64) in white_location:
+                            Selected = (x_pos//64, y_pos//64)
+                            Player_click.append(Selected)
+                        elif turn == 'black' and (x_pos//64, y_pos//64) in black_location:
+                            Selected = (x_pos//64, y_pos//64)
+                            Player_click.append(Selected)
+                    else:
+                        if turn == 'white' and (x_pos//64, y_pos//64) in white_location:
+                                circle_group.empty()
+                                Player_click.clear()
+                                Selected = (x_pos//64, y_pos//64)
+                                Player_click.append(Selected)
+
+                        elif turn == 'black' and (x_pos//64, y_pos//64) in black_location:
+                                circle_group.empty()
+                                Player_click.clear()
+                                Selected = (x_pos//64, y_pos//64)
+                                Player_click.append(Selected)
+                        else:
+                                Selected = (x_pos//64, y_pos//64)
+                                Player_click.append(Selected)
+
+            if len(Player_click) == 1:
+                    for n in gs.board[x_pos//64][y_pos//64].get_valid_moves((x_pos//64, y_pos//64), gs, turn):
+                        circle_sprite = Circle((n[1] * 64) + 32, (n[0] * 64) + 32, CIRCLE_RADIUS)
+                        circle_group.add(circle_sprite)  
+
+            if len(Player_click) == 2:
+                if turn == "white" and Player_click[0] in white_location:
+                        if move_piece(Player_click, gs):
+                            white_location.remove(Player_click[0])
+                            white_location.add(Player_click[1])
+                            turn = "black"
+
+                elif turn == "black" and Player_click[0] in black_location:
+                    if move_piece(Player_click, gs):
+                            black_location.remove(Player_click[0])
+                            black_location.add(Player_click[1])
+                            turn = "white"
+                
+                if gs.if_Check(turn):
+                    if turn == "white":
+                         for square in square_group.sprites():
+                            if square.rect.y // 64 == gs.whiteKing[0] and square.rect.x // 64 == gs.whiteKing[1]:
+                                square.image.fill('red')
+                    elif turn == "black":
+                          for square in square_group.sprites():
+                            if square.rect.y // 64 == gs.whiteKing[0] and square.rect.x // 64 == gs.whiteKing[1]:
+                                square.image.fill('red')
+                else:
+                    for square in square_group.sprites():
+                        square.image.fill(square.original)
+
+                draw_pieces(screen,gs)
                 Selected = ()
                 Player_click = []
-                circle_group.empty()
-           else:
-                if len(Player_click) == 0:
-                    if turn == 'white' and (x_pos//64, y_pos//64) in white_location:
-                        Selected = (x_pos//64, y_pos//64)
-                        print(Selected)
-                        Player_click.append(Selected)
-                    elif turn == 'black' and (x_pos//64, y_pos//64) in black_location:
-                        Selected = (x_pos//64, y_pos//64)
-                        Player_click.append(Selected)
-                else:
-                   if turn == 'white' and (x_pos//64, y_pos//64) in white_location:
-                        circle_group.empty()
-                        Player_click.clear()
-                        Selected = (x_pos//64, y_pos//64)
-                        Player_click.append(Selected)
 
-                   elif turn == 'black' and (x_pos//64, y_pos//64) in black_location:
-                        circle_group.empty()
-                        Player_click.clear()
-                        Selected = (x_pos//64, y_pos//64)
-                        Player_click.append(Selected)
-                   else:
-                        Selected = (x_pos//64, y_pos//64)
-                        Player_click.append(Selected)
-
-           if len(Player_click) == 1:
-                for n in (eval(gs.board[x_pos//64][y_pos//64]).get_moves((x_pos//64, y_pos//64), gs.board)):
-                    circle_sprite = Circle((n[1] * 64) + 32, (n[0] * 64) + 32, CIRCLE_RADIUS)
-                    circle_group.add(circle_sprite)  
-
-           if len(Player_click) == 2:
-               if turn == "white" and Player_click[0] in white_location:
-                    if move_piece(Player_click, gs):
-                        white_location.remove(Player_click[0])
-                        white_location.add(Player_click[1])
-                        turn = "black"
-
-               elif turn == "black" and Player_click[0] in black_location:
-                   if move_piece(Player_click, gs):
-                        black_location.remove(Player_click[0])
-                        black_location.add(Player_click[1])
-                        turn = "white"
-               draw_pieces(screen,gs)
-    
-               Selected = ()
-               Player_click = []
-
-          
+        
     square_group.draw(screen)
     draw_pieces(screen,gs)
 
--- classes.py
+++ classes.py
@@ -16,61 +16,147 @@ class Circle(pg.sprite.Sprite):
     def update(self):
         pg.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
 
+
 class Game_State:
-    def __init__(self,background = None):
+    def __init__(self, background=None):
         self.image = background
         self.board = [
-            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-            ['bp' for n in range(8)],
-            ["--" for n in range(8)],
-            ["--" for n in range(8)],
-            ["--" for n in range(8)],
-            ["--" for n in range(8)],
-            ['wp' for n in range(8)],
-            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+            [Castle('bR.png', 'black'), Knight('bN.png', 'black'), Bishop('bB.png', 'black') ,Queen('bQ.png', 'black'), King('bK.png', 'black'), Bishop('bB.png', 'black'), Knight('bN.png', 'black'),Castle('bR.png', 'black')],
+            [Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black'), Pawn('bp.png', 'black')],
+            ["--" for _ in range(8)],
+            ["--" for _ in range(8)],
+            ["--" for _ in range(8)],
+            ["--" for _ in range(8)],
+            [Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white'), Pawn('wp.png', 'white')],
+            [Castle('wR.png', 'white'),Knight('wN.png', 'white'),Bishop('wB.png', 'white'), Queen('wQ.png', 'white'), King('wK.png', 'white'), Bishop('wB.png', 'white'), Knight('wN.png', 'white'), Castle('wR.png', 'white')]
         ]
 
+        self.white_pieces = []
+        self.black_pieces = []
 
-    def print_board(self):
-        for n in self.board:
-            print(n)
+        self.whiteKing = None
+        self.blackKing = None
+
+    def get_pos(self,chesspiece):
+        """
+        Input type: Piece Object
+        Return type: A tuple of the x and y coords on the board
+        """
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == chesspiece:
+                    return (i,j)
+
+
+    def if_Check(self, turn):
+        if turn == "white":
+            for pieces in self.black_pieces:
+                if self.whiteKing in pieces.get_moves(self.get_pos(pieces), self.board):
+                    return True
+        elif turn == "black":
+            for pieces in self.white_pieces:
+                if self.blackKing in pieces.get_moves(self.get_pos(pieces), self.board):
+                    return True
+
+    def squareUnderAttack(self, start, turn):
+        valid_moves = []
+        piece = self.board[start[0]][start[1]]
+        if turn == "white":
+            if piece in self.white_pieces:
+                pseudo_moves = piece.get_moves(start, self.board)
+                for moves in pseudo_moves:
+                    captured = self.pseudo_move([start, moves])
+                    is_valid = True
+                    for pieces in self.black_pieces:
+                        if self.whiteKing in pieces.get_moves(self.get_pos(pieces), self.board):
+                            is_valid = False
+                    self.undo_move([start, moves], captured)
+                    if is_valid:
+                        valid_moves.append(moves)
+
+        elif turn == "black":
+            if piece in self.black_pieces:
+                pseudo_moves = piece.get_moves(start, self.board)
+                for moves in pseudo_moves:
+                    captured = self.pseudo_move([start, moves])
+                    is_valid = True
+                    for pieces in self.white_pieces:
+                        if self.blackKing in pieces.get_moves(self.get_pos(pieces), self.board):
+                            is_valid = False
+                    self.undo_move([start, moves], captured)
+                    if is_valid:
+                        valid_moves.append(moves)                        
+        return valid_moves
+
+
+    def pseudo_move(self, coords):
+        start, end = coords
+        tmp = self.board[start[0]][start[1]]
+        self.board[start[0]][start[1]] = "--"
+        captured = self.board[end[0]][end[1]]
+        self.board[end[0]][end[1]] = tmp
+
+        if tmp.color == "white" and isinstance(tmp, King):
+            self.whiteKing = end
+        elif tmp.color == "black" and isinstance(tmp, King):
+            self.blackKing = end
+        if captured in self.white_pieces:
+            self.white_pieces.remove(captured)
+        elif captured in self.black_pieces:
+            self.black_pieces.remove(captured)
+
+        return captured
+    
+
+    def undo_move(self, coords, captured_piece):
+        start, end = coords
+        piece = self.board[end[0]][end[1]]
+        self.board[start[0]][start[1]] = piece
+        self.board[end[0]][end[1]] = captured_piece
+
+        if piece.color == "white" and isinstance(piece,King):
+            self.whiteKing = start
+        elif piece.color == "black" and isinstance(piece, King):
+            self.blackKing = start
+        if captured_piece != "--":
+            if captured_piece.color == "white":
+                self.white_pieces.append(captured_piece)
+            elif captured_piece.color == "black":
+                self.black_pieces.append(captured_piece)
 
 class Piece:
     def __init__(self, image, color):
         self.image = pg.image.load(image) 
         self.color = color
 
+
+    def get_moves(self, pos, board):
+        return []
+
+    def get_valid_moves(self, pos, gs, turn):
+        if self.color == "white":
+            return gs.squareUnderAttack(pos, turn)
+        elif self.color == "black":
+            return gs.squareUnderAttack(pos, turn)
+    
 class King(Piece):
     def __init__(self, image, color):
         Piece.__init__(self, image, color)
 
     def get_moves(self, pos, board):
-        moves = []
-        x, y = pos
+            x, y = pos
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+            moves = []
 
-        # determine direction based on color
-        if self.color == "white":
-            direction = -1
-        else:
-            direction = 1
-
-        # check one square forward
-        if board[x+direction][y] == "--":
-            moves.append((x+direction, y))
-
-        if  board[x][y+1] == "--":
-                moves.append((x, y+1))
-
-        if  board[x][y-1] == "--":
-                moves.append((x, y-1))
-
-        # check diagonal capture moves
-        if y > 0 and board[x+direction][y-1][0] != self.color[0]:
-            moves.append((x+direction, y-1))
-        if y < 7 and board[x+direction][y+1][0] != self.color[0]:
-            moves.append((x+direction, y+1))
-
-        return moves
+            for dx, dy in directions:
+                x_temp, y_temp = x + dx, y + dy
+                if 0 <= x_temp < len(board) and 0 <= y_temp < len(board[x_temp]):
+                    if board[x_temp][y_temp] == "--":
+                        moves.append((x_temp, y_temp))
+                    elif board[x_temp][y_temp].color != self.color:
+                        moves.append((x_temp, y_temp))
+                    
+            return moves
         
 
 class Queen(Piece):
@@ -85,11 +171,12 @@ class Queen(Piece):
         for dx, dy in directions:
             x_temp, y_temp = x + dx, y + dy
             while 0 <= x_temp < len(board) and 0 <= y_temp < len(board[x_temp]):
-                if board[x_temp][y_temp][0] == self.color[0]:
-                    break
-                elif board[x_temp][y_temp][0] != "-":
-                    moves.append((x_temp, y_temp))
-                    break
+                if board[x_temp][y_temp] != "--":
+                    if board[x_temp][y_temp].color == self.color:
+                        break
+                    else:
+                        moves.append((x_temp, y_temp))
+                        break
                 moves.append((x_temp, y_temp))
                 x_temp += dx
                 y_temp += dy
@@ -109,11 +196,12 @@ class Castle(Piece):
         for dx, dy in directions:
             x_temp, y_temp = x + dx, y + dy
             while 0 <= x_temp < len(board) and 0 <= y_temp < len(board[x_temp]):
-                if board[x_temp][y_temp][0] == self.color[0]:
-                    break
-                elif board[x_temp][y_temp][0] != "-":
-                    moves.append((x_temp, y_temp))
-                    break
+                if board[x_temp][y_temp] != "--":
+                    if board[x_temp][y_temp].color == self.color:
+                        break
+                    else:
+                        moves.append((x_temp, y_temp))
+                        break
                 moves.append((x_temp, y_temp))
                 x_temp += dx
                 y_temp += dy
@@ -133,11 +221,12 @@ class Bishop(Piece):
         for dx, dy in directions:
             x_temp, y_temp = x + dx, y + dy
             while 0 <= x_temp < len(board) and 0 <= y_temp < len(board[x_temp]):
-                if board[x_temp][y_temp][0] == self.color[0]:
-                    break
-                elif board[x_temp][y_temp][0] != "-":
-                    moves.append((x_temp, y_temp))
-                    break
+                if board[x_temp][y_temp] != "--":
+                    if board[x_temp][y_temp].color == self.color:
+                        break
+                    else:
+                        moves.append((x_temp, y_temp))
+                        break
                 moves.append((x_temp, y_temp))
                 x_temp += dx
                 y_temp += dy
@@ -166,7 +255,7 @@ class Knight(Piece):
             if 0 <= move[0] <= 7 and 0 <= move[1] <= 7:  # make sure move is on the board
                 if board[move[0]][move[1]] == "--":  # empty square
                     moves.append(move)
-                elif board[move[0]][move[1]][0] != self.color[0]:  # capture opponent's piece
+                elif board[move[0]][move[1]].color != self.color:  # capture opponent's piece
                     moves.append(move)
 
         return moves
@@ -195,9 +284,9 @@ class Pawn(Piece):
                 moves.append((x+2*direction, y))
 
         # check diagonal capture moves
-        if y > 0 and board[x+direction][y-1] != '--' and board[x+direction][y-1][0] != self.color[0]:
+        if y > 0 and board[x+direction][y-1] != '--' and board[x+direction][y-1].color != self.color:
             moves.append((x+direction, y-1))
-        if y < 7 and board[x+direction][y+1] != '--' and board[x+direction][y+1][0] != self.color[0]:
+        if y < 7 and board[x+direction][y+1] != '--' and board[x+direction][y+1].color != self.color:
             moves.append((x+direction, y+1))
 
         return moves
@@ -209,3 +298,4 @@ class Squares(pg.sprite.Sprite):
         self.image.fill(fill)
         self.rect = self.image.get_rect()
         self.rect.topleft = [x, y]
+        self.original = fill
